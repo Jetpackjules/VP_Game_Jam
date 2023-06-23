2c63f0ed0ac02fe27326a0ad91160af3b546bed3
Jetpackjules <jetpackjules@gmail.com>
2023-06-22 17:50:57 -0700

    added shoot interval

--- Projectiles/Basic/Emitter.gd
+++ Projectiles/Basic/Emitter.gd
@@ -1,16 +1,38 @@
 extends Node2D
 
+
 export var projectile_scene = preload("res://Projectiles/Basic/Projectile.tscn")
 export var projectile_speed: float = 900.0
 export var projectile_size: Vector2 = Vector2(1, 1)
 export var amount_emitted: int = 1
 
+export var fan_angle: float = 360.0
+export var origin_angle: float = 0.0
+export var firing_interval: float = 1.0
+
+var emit_timer = Timer.new()
+
+func _ready():
+	# Add the timer as a child of this node and set it up
+	add_child(emit_timer)
+	emit_timer.set_wait_time(1.0)  # emit every 1.0 seconds, adjust as needed
+	emit_timer.set_one_shot(false)  # repeat the timer
+	emit_timer.connect("timeout", self, "_on_emit_timer_timeout")
+	emit_timer.start()
+
+func _on_emit_timer_timeout():
+	emit_projectiles()
+
+func set_interval(input_interval):
+	firing_interval = input_interval
+	emit_timer.wait_time = firing_interval
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_fire"):
 		emit_projectiles()
 
-export var fan_angle: float = 360.0
-export var origin_angle: float = 0.0
+#export var fan_angle: float = 360.0
+#export var origin_angle: float = 0.0
 
 func emit_projectiles():
 	var angle_step = deg2rad(fan_angle) / max(amount_emitted, 1)
@@ -19,7 +41,6 @@ func emit_projectiles():
 	for i in range(amount_emitted):
 		var projectile = projectile_scene.instance()
 		projectile.global_position = global_position
-#		projectile.set_speed(projectile_speed)
 		projectile.set_size(projectile_size)
 		var angle = start_angle + i * angle_step
 		projectile.set_velocity(Vector2(cos(angle), sin(angle)) * projectile_speed)
