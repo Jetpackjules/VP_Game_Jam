7851c40374f16d552cb39370711f8780e5c495d4
Jetpackjules <jetpackjules@gmail.com>
2023-06-27 16:03:02 -0700

    All done!

--- Menu.gd
+++ Menu.gd
@@ -15,6 +15,19 @@ func reset():
 	score.card_checkpoint = 0
 	player_health.set_health(100)
 
+	# Assuming the script is attached to a Node named 'MyNode'
+#	var PlayerScene = preload("res://Player/Player.tscn") # Load the scene or node
+#
+#	# To reset the node:
+#	var parentNode = player.get_parent() # Get the parent of the current node
+##	var myNodePos = player.get_position() # Store the old position if needed
+#	player.queue_free() # Free the current node
+#
+#	# Create a new instance of the node and add it to the scene tree
+#	var player_new = PlayerScene.instance()
+#	parentNode.add_child(player_new)
+#	newMyNode.set_position(myNodePos) # Set the position to the old position
+
 	enemy_spawner.speed_increase = 0
 	enemy_spawner.health_increase = 0
 	enemy_spawner.spawn_interval = 5.0
