ef698a5219d3e6e70a10c5ef1f23f944d002e612
Jetpackjules <jetpackjules@gmail.com>
2023-07-07 13:24:53 -0700

    improved sniper

--- Enemies/Sniper.gd
+++ Enemies/Sniper.gd
@@ -14,6 +14,8 @@ onready var laser_sight = $Laser_Sight
 onready var polygon2D = $Polygon2D  # Reference to the Polygon2D node
 onready var laser_raycast = $Polygon2D/Laser_Raycast  # Reference to the Laser_Raycast node
 
+var last_known_player_position = Vector2()
+
 func _ready():
 	laser_sight.points[1] = Vector2.ZERO  # Hide the laser sight
 
@@ -22,10 +24,15 @@ func _physics_process(delta):
 	var aim_direction = Vector2(cos(polygon2D.rotation), sin(polygon2D.rotation))
 	var angle_to_player = acos(aim_direction.dot(player_direction)) * 180 / PI
 
+	if player_tracker.player_visible:
+		last_known_player_position = player_tracker.player.global_position
+
 	match state:
 		State.IDLE:
 			if player_tracker.player_visible:
 				state = State.AIMING
+			else:
+				rotate_polygon_towards_last_known_position(delta, AIM_SPEED)
 		State.AIMING:
 			if player_tracker.player_visible:
 				rotate_polygon_towards_player(delta, AIM_SPEED)
@@ -55,6 +62,11 @@ func rotate_polygon_towards_player(delta, speed):
 	var target_rotation = player_direction.angle()
 	polygon2D.rotation = lerp_angle(polygon2D.rotation, target_rotation, speed * delta)
 
+func rotate_polygon_towards_last_known_position(delta, speed):
+	var direction = (last_known_player_position - global_position).normalized()
+	var target_rotation = direction.angle()
+	polygon2D.rotation = lerp_angle(polygon2D.rotation, target_rotation, speed * delta)
+
 func update_laser_sight():
 	laser_raycast.force_raycast_update()
 	if laser_raycast.is_colliding():
@@ -63,5 +75,19 @@ func update_laser_sight():
 		laser_sight.points[1] = laser_raycast.cast_to
 
 func fire():
-	# Implement your firing logic here
-	pass
+	# Make the laser wide and bright red
+	laser_sight.width = 10  # Adjust the width as needed
+	laser_sight.default_color = Color(1, 0, 0, 1)  # Bright red color
+
+	# Check if the raycast is colliding with the player
+	laser_raycast.force_raycast_update()
+	if laser_raycast.is_colliding():
+		var collider = laser_raycast.get_collider()
+		if collider == player_tracker.player:
+			# Call the player's hit() function
+			collider.hit()  # Make sure the player has a hit() function
+
+	# Reset the laser sight after firing
+	yield(get_tree().create_timer(0.5), "timeout")  # Wait for 0.5 seconds
+	laser_sight.width = 5  # Reset the width
+	laser_sight.default_color = Color(1, 0, 0, 0.5)  # Reset the color
